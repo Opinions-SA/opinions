445d108468101c7780d08026f84b24e2e94c0e54
Murilo C Galina <murilochiquettigalina@gmail.com>
2023-10-23 21:44:47 -0300

    OP-18 back feat: Bringing tmdb API integration to the back. Creating classes, controllers, services, repositories and clientServices.

--- opinions-back/src/main/java/com/opinions/OpinionsApplication.java
+++ opinions-back/src/main/java/com/opinions/OpinionsApplication.java
@@ -2,8 +2,12 @@ package com.opinions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@ServletComponentScan("com.opinions")
+@EnableFeignClients
 public class OpinionsApplication {
 
 	public static void main(String[] args) {
