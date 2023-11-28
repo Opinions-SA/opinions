f1d56820e6786a79a09b1ff0d94dde212f6eb040
GabrielCarvalhouo <gabrielvasconcellos76@gmail.com>
2023-11-28 17:10:44 -0300

    Fix: Adding new style and positioning for TvSerie

--- opinions-front/src/pages/TvSerie.tsx
+++ opinions-front/src/pages/TvSerie.tsx
@@ -51,93 +51,98 @@ const TvSeriePage = () => {
     <div className="tvSerie-page">
       {tvSerie && (
         <>
-        <div className="card-container-movies">
-          <div className="card-content-movies">
-          <div className="poster-content-movie">
-          {tvSerie.poster_path && (
-            <img
-              className="poster-serie-image"
-              src={imageUrl + tvSerie.poster_path}
-              alt="serie.title"
-              style={{ width: "100%", maxHeight: "100%" }}
-            />
-          )}
-          </div>
-          <div className="info-serie-container">
-              <h1 className="serie-title">{tvSerie.name}</h1>
-              <p className="serie-tag">{tvSerie.tagline}</p>
-              <div className="info-series">
-
-            <div className="info-card">
-              <h3>
-                <BsWallet2 /> Genres
-              </h3>
-              <div className="genres">
-                {tvSerie.genres.map((genre) => (
-                  <p className="genre"> {genre.name}</p>
-                ))}
+          <div className="card-container-series">
+            <div className="card-content-series">
+              <div className="poster-content-serie">
+                {tvSerie.poster_path && (
+                  <img
+                    className="poster-serie-image"
+                    src={imageUrl + tvSerie.poster_path}
+                    alt="serie.title"
+                    style={{ width: "100%", maxHeight: "100%" }}
+                  />
+                )}
+              </div>
+              <div className="info-series-container">
+                <h1 className="serie-title">{tvSerie.name}</h1>
+                <p className="serie-tag">{tvSerie.tagline}</p>
+                <div className="info-series">
+                  <div className="info-card">
+                    <h3>
+                      <BsWallet2 /> Genres
+                    </h3>
+                    <div className="genres">
+                      {tvSerie.genres.map((genre) => (
+                        <p className="genre"> {genre.name}</p>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="info-card">
+                    <h3>
+                      <BsHourglassSplit /> First air date
+                    </h3>
+                    <p>{formatDate(tvSerie.first_air_date)}</p>
+                  </div>
+                  <div className="info-card">
+                    <h3>
+                      <BsHourglassSplit /> Last air date
+                    </h3>
+                    <p>{formatDate(tvSerie.last_air_date)}</p>
+                  </div>
+                  <div className="info-card">
+                    <h3>
+                      <BsHourglassSplit />{" "}
+                      {tvSerie.in_production ? "In production" : "Ended"}
+                    </h3>
+                  </div>
+                  <div className="info-card">
+                    <h3>
+                      <BsWallet2 /> Networks
+                    </h3>
+                    <div className="genres-series">
+                      {tvSerie.networks.map((network) => (
+                        <p className="genre">
+                          {" "}
+                          <img
+                            className="network-image"
+                            src={imageUrl + network.logo_path}
+                          />
+                        </p>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="info-card">
+                    <h3>
+                      <BsHourglassSplit /> Season number
+                    </h3>
+                    <p>{tvSerie.number_of_seasons}</p>
+                  </div>
+                  <div className="info-card">
+                    <h3>
+                      <BsHourglassSplit /> Episode number
+                    </h3>
+                    <p>{tvSerie.number_of_episodes}</p>
+                  </div>
+                </div>
+                <div className="info-description">
+                  <h3>
+                    <BsFillFileEarmarkTextFill /> Description
+                  </h3>
+                  <p>{tvSerie.overview}</p>
+                </div>
+                <button
+                  className="open-review-button"
+                  onClick={toggleUserReview}
+                >
+                  Abrir Avaliação
+                </button>
+                {showUserReview && (
+                  <div className="review-overlay">
+                    <UserReview onClose={toggleUserReview} />
+                  </div>
+                )}
               </div>
             </div>
-            <div className="info-card">
-              <h3>
-                <BsHourglassSplit /> First air date
-              </h3>
-              <p>{formatDate(tvSerie.first_air_date)}</p>
-            </div>
-            <div className="info-card">
-              <h3>
-                <BsHourglassSplit /> Last air date
-              </h3>
-              <p>{formatDate(tvSerie.last_air_date)}</p>
-            </div>
-            <div className="info-card">
-              <h3>
-                <BsHourglassSplit />{" "}
-                {tvSerie.in_production ? "In production" : "Ended"}
-              </h3>
-            </div>
-            <div className="info-card">
-              <h3>
-                <BsWallet2 /> Networks
-              </h3>
-              <div className="genres-series">
-                {tvSerie.networks.map((network) => (
-                  <p className="genre">
-                    {" "}
-                    <img className="network-image" src={imageUrl + network.logo_path} />
-                  </p>
-                ))}
-              </div>
-            </div>
-            <div className="info-card">
-              <h3>
-                <BsHourglassSplit /> Season number
-              </h3>
-              <p>{tvSerie.number_of_seasons}</p>
-            </div>
-            <div className="info-card">
-              <h3>
-                <BsHourglassSplit /> Episode number
-              </h3>
-              <p>{tvSerie.number_of_episodes}</p>
-            </div>
-            </div>
-          <div className="info-description">
-            <h3>
-              <BsFillFileEarmarkTextFill /> Description
-            </h3>
-            <p>{tvSerie.overview}</p>
-          </div>
-          <button className="open-review-button" onClick={toggleUserReview}>
-            Abrir Avaliação
-          </button>
-          {showUserReview && (
-            <div className="review-overlay">
-              <UserReview onClose={toggleUserReview} />
-            </div>
-          )}
-          </div>
-          </div>
           </div>
         </>
       )}
