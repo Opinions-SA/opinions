edf0c6a4be4e5e1fdec1a4a81b21e92cb16c9f6b
GabrielCarvalhouo <gabrielvasconcellos76@gmail.com>
2023-11-29 14:26:44 -0300

    fix: ListReview created with base structure

---
+++ opinions-front/src/components/userReview/ListReview.tsx
@@ -0,0 +1,14 @@
+const ListReview = () => {
+  return (
+    <div className="review-list-container">
+      <h2>Recent Reviews</h2>
+      <ul>
+          <li>
+            <strong>title</strong> - description (Rating: )
+          </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ListReview;
